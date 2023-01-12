6cfdfb0b75efc219f6c82b117962fbc05bee1d52
RaphaelMW <raphael@mundowap.com.br>
2023-01-12 16:24:02 -0300

    add user update by admin

---
+++ src/modules/users/useCases/updateUserAmin/UpdateUserAdminUseCase.spec.ts
@@ -0,0 +1,32 @@
+import { ICreateUser } from "@modules/users/interfaces/ICreateUser";
+import { UserRepositoryInMemory } from "@modules/users/repositories/in-memory/UserRepositoryInMemory";
+import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { UpdateUserAdminUseCase } from "./UpdateUserAdminUseCase";
+
+let userRepository: IUsersRepository;
+let updateUserAdminUseCase: UpdateUserAdminUseCase;
+let createUser: CreateUserUseCase;
+
+describe("Update a user by id", () => {
+  beforeEach(() => {
+    userRepository = new UserRepositoryInMemory();
+    updateUserAdminUseCase = new UpdateUserAdminUseCase(userRepository);
+    createUser = new CreateUserUseCase(userRepository);
+  });
+
+  it("should be able update an user by id", async () => {
+    await createUser.execute({
+      name: "John",
+      email: "John@due.com",
+      password: "1234",
+    });
+
+    const user = await userRepository.findByEmail("John@due.com");
+
+    await updateUserAdminUseCase.execute({ id: user.id, name: "Lucca" });
+
+    const updatedUser = await userRepository.findByEmail("John@due.com");
+    expect(updatedUser.name).toBe("Lucca");
+  });
+});
